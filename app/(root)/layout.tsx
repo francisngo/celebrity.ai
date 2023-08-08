c449e4111141a0882d67c8bc5b1b3765431712f6
Francis Ngo <francis.t.ngo@gmail.com>
2023-08-08 15:21:36 -0700

    Fix missing 'isPro' required prop

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,11 +2,12 @@ import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+	const isPro = true;
 	return (
 		<div className="h-full">
-			<Navbar />
+			<Navbar isPro={isPro} />
 			<div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
-				<Sidebar />
+				<Sidebar isPro={isPro} />
 			</div>
 			<main className="md:pl-20 pt-16 h-full">{children}</main>
 		</div>
