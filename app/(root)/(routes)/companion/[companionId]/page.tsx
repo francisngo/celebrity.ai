a818af2221f69cb66ec1ddf129a72cb550d2accb
Francis Ngo <francis.t.ngo@gmail.com>
2023-08-11 14:03:48 -0700

    Begin building companion form UI

---
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -0,0 +1,23 @@
+import prisma from "@/lib/prisma";
+import { CompanionForm } from "./components/companion-form";
+
+interface CompanionIdPageProps {
+	params: {
+		companionId: string;
+	};
+}
+
+const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
+	// TODO: Check subscription logic here
+	const companion = await prisma.companion.findUnique({
+		where: {
+			id: params.companionId,
+		},
+	});
+
+	const categories = await prisma.category.findMany();
+
+	return <CompanionForm initialData={companion} categories={categories} />;
+};
+
+export default CompanionIdPage;
