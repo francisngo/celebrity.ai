9199f87dde36337441d11dba77124fe8f0a24ed8
Francis Ngo <francis.t.ngo@gmail.com>
2023-09-05 09:38:24 -0700

    Add Celebrities List component

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,14 +1,41 @@
 import { SearchInput } from "@/app/components/search-input";
+import { Categories } from "@/app/components/categories";
+import { Celebrities } from "@/app/components/celebrities";
 import prisma from "@/app/lib/prisma";
 
-import { Categories } from "@/app/components/categories";
+interface RootPageProps {
+	searchParams: {
+		categoryId: string;
+		name: string;
+	};
+}
 
-const RootPage = async () => {
+const RootPage = async ({ searchParams }: RootPageProps) => {
 	const categories = await prisma.category.findMany();
+	const celebrities = await prisma.celebrity.findMany({
+		where: {
+			categoryId: searchParams.categoryId,
+			name: {
+				search: searchParams.name,
+			},
+		},
+		orderBy: {
+			createdAt: "desc",
+		},
+		include: {
+			_count: {
+				select: {
+					messages: true,
+				},
+			},
+		},
+	});
+
 	return (
 		<div className="h-full p-4 space-y-2">
 			<SearchInput />
 			<Categories data={categories} />
+			<Celebrities data={celebrities} />
 		</div>
 	);
 };
